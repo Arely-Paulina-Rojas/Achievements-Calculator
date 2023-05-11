6f77ee83f11b6696717454af6d2d8c7b93dd0749
arely_paulina <arely1934@outlook.com>
2023-05-11 17:49:30 -0600

    Added "AlreadyHaveAnAccount" component

---
+++ achievements_calculator/lib/components/already_have_an_account_check.dart
@@ -0,0 +1,33 @@
+import 'package:achievements_calculator/components/gradient_text.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class AlreadyHaveAnAccountCheck extends StatelessWidget {
+  final bool login;
+  final void Function()? press;
+  const AlreadyHaveAnAccountCheck({Key? key, this.login = true, this.press})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          login ? "Don’t have an account? " : "Already have an account?",
+          style: const TextStyle(color: lightTextColor),
+        ),
+        GestureDetector(
+          onTap: press,
+          child: GradientText(
+            key,
+            login ? "Sign Up" : "Sign In",
+            style: const TextStyle(fontWeight: FontWeight.bold),
+            gradient: const LinearGradient(
+                colors: [lightMainButtonColor, darkMainButtonColor]),
+          ),
+        ),
+      ],
+    );
+  }
+}
